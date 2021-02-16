e5e60afb17d88865c716b6830f09503da1f8db42
Rosa Ekström <rjosih@hotmail.com>
2021-02-16 22:32:03 +0100

    changed url

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,17 +4,13 @@ import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
 
 import spotifyLogo from '../images/spotify.png'
-// const {
-//   REACT_APP_CLIENT_ID,
-//   REACT_APP_AUTHORIZE_URL,
-//   REACT_APP_REDIRECT_URL
-// } = process.env
+const {
+  REACT_APP_CLIENT_ID,
+  REACT_APP_AUTHORIZE_URL,
+  REACT_APP_REDIRECT_URL
+} = process.env
 
 const Home = (props) => {
-
-  var REACT_APP_CLIENT_ID='fe8ef5c68ca741bdb4f4371bf8b5759a'
-  var REACT_APP_AUTHORIZE_URL='https://accounts.spotify.com/authorize'
-  var REACT_APP_REDIRECT_URL='http://localhost:3000/redirect'
 
   const handleLogin = () => {
     window.location = `${REACT_APP_AUTHORIZE_URL}?client_id=${REACT_APP_CLIENT_ID}&redirect_uri=${REACT_APP_REDIRECT_URL}&response_type=token&show_dialog=true`;
